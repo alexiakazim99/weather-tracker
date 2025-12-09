b6c16cf0bc9a8ff1c6c7239345eb0dfc27f8d3ae
Alexia <alexiakazim99@gmail.com>
2025-12-09 14:54:52 +0100

    function to save data inserted

--- database.py
+++ database.py
@@ -5,6 +5,43 @@ def init_db()
     connection = sqlite3.connect("weather.db")
     # öppna en koppling till en databasfil som  då heter (weather.db)
     cursor = connection.cursor()
-    
+    # gör det möjligt att skriva sql här 
 
-crete table 
+crete table if not exist weather (
+    id integer primary key autoincrement, 
+    city text not null, 
+    temperatur real not null, 
+    data text not null 
+)
+
+def save_weather (city, temperature, date):
+    #funktion tar emot 3 parametrar (city, temprature, date)
+    conn = sqlite3.connect('weather.db')
+    #skapar en anslutning till databasfilen (wather.db)
+    cursor = conn.cursor() 
+
+    cursor.execute('''
+        INSERT INTO weather (city, temperature, date)
+        VALUES (?, ?, ?)
+    ''', (city, temperature, date))
+    # använder verktyget för att kunna skriva commits 
+
+    conn.commit()
+    #sparar det jag ahr skrivit 
+    conn.close() 
+    # stäng dörren 
+
+def get_all_wather(): 
+   #ansluter till databasen 
+    conn = sqlite3.connect ('weather.db')
+    cursor = conn.cursor()
+
+    # hämtar all värder data från taballen 
+    cursor.execute('SELECT * FROM weather')
+    #hämtar alla rader från en lista 
+    rows = cursor.fetchall()
+    #stäng anslutning 
+    conn.close()
+    #retunerar datan 
+    return rows 
+
