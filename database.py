bc77591f3141f4c4107a94f4e708eea4373daca3
Alexia <alexiakazim99@gmail.com>
2025-12-19 17:43:35 +0100

    more information implemented

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 # databasmotror som ligger redan inbyggdd i python, gör det möjligt att jobba med databasen 
 
 def init_db():
-    try:
+    try: # försöker köra koden 
         # Anslut till databasen (skapar weather.db om den inte finns)
         conn = sqlite3.connect('weather.db')
         cursor = conn.cursor()
@@ -23,12 +23,13 @@ def init_db():
         conn.close()
         print(" Database initalized")
         
+     #fångar alla SQLlite- relaterade fel, "e" innehåller felmedelandet
     except sqlite3.Error as e:
         print(f" Database initialization error: {e}")
 
 
 def save_weather (city, temperature, date):
-    try: 
+    try: #försöker köra datan
     #funktion tar emot 3 parametrar (city, temprature, date)
         conn = sqlite3.connect('weather.db')
          #skapar en anslutning till databasfilen (wather.db)
@@ -48,12 +49,13 @@ def save_weather (city, temperature, date):
         return True
 
     except sqlite3.Error as e:
-        print(f" Error while saving: {e}")
+        print(f" Error while saving: {e}")  # print - visar output 
         return False
-
  
-def get_all_wather(): 
-    try:
+    # return true/false = retunerar om det gick bra eller ej 
+ 
+def get_all_weather(): 
+    try: #försöker köra datan 
    #ansluter till databasen 
         conn = sqlite3.connect ('weather.db')
         cursor = conn.cursor()
@@ -71,4 +73,10 @@ def get_all_wather():
     except sqlite3.Error as e: 
        print (f" Error while saving: {e}")
        return []
+    # return [] = retunerar tom lista om fel uppstår 
+    # förhindrar att programmet krachar 
   
+
+if __name__ == "__main__": # kontrollerar om denna fil körs dirket (inte importerad från annan fil)
+    init_db() # funktionen skapar databasen och tabellen 
+    print("Database initialized") # skriver uti terminalen 
